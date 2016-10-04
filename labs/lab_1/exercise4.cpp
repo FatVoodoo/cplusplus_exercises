be2cbf6ec3f9ad38a4ee1ad9d288eab07b1169bd
Gregory kelleher <gregory@kepler.local>
2016-10-04 17:54:23 +0100

    finished exercise 4

--- labs/lab_1/exercise4.cpp
+++ labs/lab_1/exercise4.cpp
@@ -5,20 +5,45 @@
 #include <string>
 using namespace std; 
 
-const int name_len = 20;
-const int account_len = 8;
-const int balance_len = 10000;
-const int credit_lim = 500;
-
 struct customer 
 {
-    string name;
-    string account_num;
+    int account_num;
+    int balance;
+    int item_tot;
+    int credit_tot;
+    int credit_lim;
 };
 
 
 int main() {
 
-    return 0;
+    customer customer;
 
+    for(;;) 
+    {
+
+        cout << "Enter account number: ";
+        cin >> customer.account_num;
+        if (customer.account_num == -1) break;
+        cout << "Enter balance: ";
+        cin >> customer.balance;
+        cout << "Enter total items charged: ";
+        cin >> customer.item_tot;
+        cout << "Enter credit total: ";
+        cin >> customer.credit_tot;
+        cout << "Enter credit limit: ";
+        cin >> customer.credit_lim;
+
+        int balance_new = customer.balance + customer.item_tot - customer.credit_tot;
+
+        if (balance_new > customer.credit_lim) 
+        {
+            customer.balance = balance_new;
+            cout << "\nDisplaying Information." << '\0';
+            cout << "\nAccount Number: " << customer.account_num << '\0';
+            cout << "\nCredit Limit: " << customer.credit_lim << '\0';
+            cout << "\nCredit Limit exceeded" << '\0' << '\n';
+        }
+    }
+    return 0;
 }
