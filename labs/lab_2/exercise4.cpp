bb1a97969b09f8a0aee36f285409453625dd9480
Gregory kelleher <gregory@kepler.local>
2016-10-13 11:50:04 +0100

    finished exercise4 and exercise5

--- labs/lab_2/exercise4.cpp
+++ labs/lab_2/exercise4.cpp
@@ -9,40 +9,56 @@ void my_string_cat(char* dest, char* src, int dest_size);
 
 int main(void) {
 
-    char word_1[20] = "hello";
+    char word_1[12] = "hello";
     char word_2[6] = "world";
 
-    int dest_size = my_string_len(word_1);
+    /* to get real size of array */
 
-    my_string_cat(word_1, word_2, dest_size);
+    cout << "word_1 length: " << my_string_len(word_1) << '\n';
+    cout << "word_2 length: " << my_string_len(word_2) << '\n';
+
+    int concat_len = my_string_len(word_1) + my_string_len(word_2);
+
+    cout << "combined length of word_1 and word_2: " << concat_len << '\n';
+    cout << "concatenated string is: ";
+    my_string_cat(word_1, word_2, sizeof(word_1));
 
     return 0;
 }
 
 void my_string_cat(char* dest, char* src, int dest_size) {
-    
-    int index = 0;
 
-    for(int i = 0; i < dest_size; i++) {
-        if(dest[i] == '\0') {
-            for (int j = i; j < dest_size; j++) {
-               dest[j] = src[index]; 
-               index++;
+    int index = 0;
+    int concat_len = my_string_len(src) + my_string_len(dest);
+
+    if (concat_len < dest_size + 1) {
+        for(int i = 0; i < dest_size; i++) {
+            if(dest[i] == '\0') {
+                for (int j = i; j < dest_size; j++) {
+                    if(src[index] != '\0') {
+                        dest[j] = src[index]; 
+                        index++;
+                    }
+                }
             }
         }
+        for(int i = 0; i < dest_size; i++) { 
+            cout << dest[i]; 
+        }
+        cout << '\n';
     }
-
-    for(int i = 0; i < dest_size; i++ ) {
-        cout << dest[i]; 
+    else if(concat_len > dest_size) { 
+        cout << "dest string does not have enough capacity to store both strings" << '\n';
     }
 
 }
 
 int my_string_len(char str[]) {
 
-    int count = 0;
+    /* to get actual size of array */
 
+    int count = 0;
     for(int i=0; str[i]!='\0'; i++) { count++; }
-  
     return count + 1;
+
 }
